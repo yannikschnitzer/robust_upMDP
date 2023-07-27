415ae569920f5ac3841896218ec698b73d794d09
lukearcus <rickard@robots.ox.ac.uk>
2023-07-27 16:24:11 +0100

    Implemented Porter-Nudelman-Shoham algorithm for MNE solving

--- main/solvers.py
+++ main/solvers.py
@@ -102,7 +102,7 @@ def find_grad(model, pol, worst_sample):
     scaled_grad = np.copy(grad)
     scaled_grad[nonzero_inds] -= min_elem
     scaled_grad /= max_elem-min_elem
-    return grad
+    return scaled_grad
 
 def solve_subgrad(samples, model, max_iters=500, quiet=False):
     if not quiet:
@@ -142,6 +142,11 @@ def solve_subgrad(samples, model, max_iters=500, quiet=False):
     best_worst_pol = test_pol(model, [samples[worst]])[2][0]
     test_wc, test_probs, _ = test_pol(model, samples, best_worst_pol, paramed_models = sample_trans_probs)
     test_worst = np.argwhere(test_probs[:,model.Init_state]==test_wc).flatten()
+    if worst in test_worst:
+        info = {"hist":test_wc, "all":test_probs[:, model.Init_state]}
+        if not quiet:
+            print("Worst case holds with deterministic policy, deterministic is optimal")
+        return test_wc, best_worst_pol, test_worst, info
     pol = 0.1*pol + 0.9*best_worst_pol # a nicer start point
     wc, true_probs, _ = test_pol(model, samples, pol, paramed_models = sample_trans_probs)
     worst = np.argwhere(true_probs[:,model.Init_state]==wc)
@@ -152,11 +157,6 @@ def solve_subgrad(samples, model, max_iters=500, quiet=False):
     else:
         best = 1
 
-    if worst in test_worst:
-        import pdb; pdb.set_trace()
-        if not quiet:
-            print("Worst case holds with deterministic policy, deterministic is optimal")
-        return test_wc, best_worst_pol, test_wc, test_worst
     for i in tqdm(range(max_iters)):
         time_start = time.perf_counter()
         
@@ -217,13 +217,15 @@ def solve_subgrad(samples, model, max_iters=500, quiet=False):
         best_hist.append(best)
         logging.info("Current value: {:.6f}, with sample {}".format(wc, worst))
         logging.info("Policy inf norm change: {:.3f}".format(np.linalg.norm(pol-old_pol, ord=np.inf)))
-    
+        
+    wc, true_probs, _ = test_pol(model, samples, best_pol, paramed_models = sample_trans_probs)
     if model.opt == "max":
         active_sg = np.argwhere(true_probs[:, model.Init_state] <= wc+1e-3)
     else:
         active_sg = np.argwhere(true_probs[:, model.Init_state] >= wc-1e-3)
-    
-    return best, best_pol, best_hist, active_sg
+    info = {"hist":best_hist, "all":true_probs[:, model.Init_state]}
+
+    return best, best_pol, active_sg, info
 
 def find_all_pols(model):
     base_vec = [False for i in model.Actions]
@@ -262,15 +264,15 @@ def build_init_payoff(samples, model):
         dommed = False
         for i, elem in enumerate(all_probs):
             if model.opt == "max":
-                if np.all(probs <= elem):
+                if np.all(probs < elem):
                     dommed = True
-                elif np.all(probs >= elem):
+                elif np.all(probs > elem):
                     all_probs.pop(i)
                     non_dommed_pols.pop(i)
             else:
-                if np.all(probs >= elem):
+                if np.all(probs > elem):
                     dommed = True
-                elif np.all(probs <= elem):
+                elif np.all(probs < elem):
                     all_probs.pop(i)
                     non_dommed_pols.pop(i)
         if not dommed:
@@ -296,23 +298,116 @@ def calc_payoff_mat(samples, model):
     non_domed_payoffs = payoffs[:, non_domed_samples]
     return non_domed_payoffs, pols, non_domed_samples
 
+def Feas_prog(payoffs, S_pol, S_adv):
+    all_pol = list(range(payoffs.shape[0]))
+    all_adv = list(range(payoffs.shape[1]))
+    
+    neg_S_pol = [elem for elem in all_pol if elem not in S_pol]
+    neg_S_adv = [elem for elem in all_adv if elem not in S_adv]
+
+    pol_strat = cp.Variable(payoffs.shape[0])
+    adv_strat = cp.Variable(payoffs.shape[1])
+    val = cp.Variable(2)
+    objective = cp.Minimize(np.ones((1,2))@val)
+    
+    S_pol_vec = np.zeros(payoffs.shape[0])
+    for i in S_pol:
+        S_pol_vec[i] = 1
+    S_adv_vec = np.zeros(payoffs.shape[1])
+    for i in S_adv:
+        S_adv_vec[i] = 1
+    neg_S_pol_vec = np.zeros(payoffs.shape[0])
+    for i in neg_S_pol:
+        neg_S_pol_vec[i] = 1
+    neg_S_adv_vec = np.zeros(payoffs.shape[1])
+    for i in neg_S_adv:
+        neg_S_adv_vec[i] = 1
+    constraints = [
+                    pol_strat >= 0,
+                    adv_strat >= 0,
+                    sum(pol_strat) == 1,
+                    sum(adv_strat) == 1,
+                    pol_strat@S_pol_vec == 1,
+                    adv_strat@S_adv_vec == 1,
+                    ]
+    for i in S_adv:
+        constraints.append((pol_strat@payoffs)[i] == val[1])
+    for i in S_pol:
+        constraints.append((payoffs@adv_strat)[i] == val[0])
+    for i in neg_S_adv:
+        constraints.append((pol_strat@payoffs)[i] >= val[1])
+    for i in neg_S_pol:
+        constraints.append((payoffs@adv_strat)[i] <= val[0])
+    prob = cp.Problem(objective, constraints)
+    try:
+        result = prob.solve()
+    except SolverError:
+        return None, None
+    if result is not np.inf:
+        return (pol_strat.value, adv_strat.value), val.value[0]
+    else:
+        return None, None
+
+def PNS_algo(payoffs):
+    strat_lengths = payoffs.shape
+    x_1_poss = list(range(1, strat_lengths[0]+1))
+    x_2_poss = list(range(1, strat_lengths[1]+1))
+    comb_poss = []
+    max_sum = 0
+    for x_1 in x_1_poss:
+        for x_2 in x_2_poss:
+            comb_poss.append((x_1, x_2))
+            max_sum = max(max_sum, x_1+x_2)
+    sorter = lambda e: np.abs(e[0]-e[1])*(max_sum+1)+e[0]+e[1]
+    comb_poss.sort(key=sorter)
+    for x_1, x_2 in tqdm(comb_poss):
+        supps_pols = list(itertools.combinations(range(strat_lengths[0]), x_1))
+        for S_pol in supps_pols:
+            redux_payoffs = payoffs[S_pol, :]
+            Adv_act_prime = []
+            for i, elem in enumerate(redux_payoffs.T):
+                if not np.any(np.all(elem.T[:,np.newaxis] > redux_payoffs, axis=0)):
+                    # elem not dominated
+                    Adv_act_prime.append(i)
+            redux_redux_payoffs = redux_payoffs[:, Adv_act_prime]
+            conditional_dommed_exists = False
+            for i, elem in enumerate(redux_redux_payoffs):
+                if np.any(np.all(elem[np.newaxis, :] < redux_redux_payoffs, axis=1)):
+                    conditional_dommed_exists = True
+            if not conditional_dommed_exists:
+                supps_adv = list(itertools.combinations(Adv_act_prime, x_2))
+                for S_adv in supps_adv:
+                    r_r_r_payoffs = redux_payoffs[:, S_adv]
+                    conditional_dommed_exists_2 = False
+                    for i, elem in enumerate(r_r_r_payoffs):
+                        if np.any(np.all(elem[np.newaxis, :] < r_r_r_payoffs, axis=1)):
+                            conditional_dommed_exists_2 = True
+                    if not conditional_dommed_exists_2:
+                        pol, val = Feas_prog(payoffs, S_pol, S_adv)
+                        if pol is not None:
+                            return pol, val
+
 def MNE_solver(samples, model):
     payoffs, pols, rel_samples = calc_payoff_mat(samples, model)
-    best = 0
-    print("---------------------\nIterating through MNE combinations")
-    combs = list(itertools.combinations(range(len(rel_samples)), len(pols)))
-    for elem in tqdm(combs):
-        test_non = elem
-        test_non_domed_payoffs = payoffs[:, test_non]
-        br_mixer = np.ones((1, len(pols)))@np.linalg.inv(test_non_domed_payoffs)
-        br_mixer = np.maximum(0, br_mixer)
-        br_mixer /= np.sum(br_mixer)
-        res = (br_mixer@payoffs).flatten()
-        if min(res) > best:
-            best = min(res)
-            best_samples = elem
-            best_pol = np.copy(br_mixer)
-    return best, best_pol, pols 
+    if model.opt != "max":
+        payoffs = -payoffs
+    pol, val = PNS_algo(payoffs)
+    #best = 0
+    #print("---------------------\nIterating through MNE combinations")
+    #combs = list(itertools.combinations(range(len(rel_samples)), len(pols)))
+    #for elem in tqdm(combs):
+    #    test_non = elem
+    #    test_non_domed_payoffs = payoffs[:, test_non]
+    #    br_mixer = np.ones((1, len(pols)))@np.linalg.inv(test_non_domed_payoffs)
+    #    br_mixer = np.maximum(0, br_mixer)
+    #    br_mixer /= np.sum(br_mixer)
+    #    res = (br_mixer@payoffs).flatten()
+    #    if min(res) > best:
+    #        best = min(res)
+    #        best_samples = elem
+    #        best_pol = np.copy(br_mixer)
+    info = {"pols": pol, "all":(pol[0]@payoffs).flatten(), "ids":rel_samples}
+    return val, pol[0], info
 
 def FSP_solver(samples, model, max_iters = 100000):
     update_parallel = True
@@ -333,13 +428,22 @@ def FSP_solver(samples, model, max_iters = 100000):
             pol_vec = payoffs@sample_dist
             
             pol_dist *= old
-            pol_dist[0, np.argmax(pol_vec)] += new
-            
             sample_dist *= old
-            sample_dist[np.argmin(sample_vec), 0] += new
-            
-    res = min((pol_dist@payoffs).flatten())
-    return res, pol_dist, pols, np.sum(sample_dist >= 1e-4)
+            if model.opt == "max":
+                pol_dist[0, np.argmax(pol_vec)] += new
+                sample_dist[np.argmin(sample_vec), 0] += new
+            else:
+                pol_dist[0, np.argmin(pol_vec)] += new
+                sample_dist[np.argmax(sample_vec), 0] += new
+
+
+    if model.opt == "max":
+        res = min((pol_dist@payoffs).flatten())
+    else:
+        res = max((pol_dist@payoffs).flatten())
+
+    info = {"pols": pols, "all":(pol_dist@payoffs).flatten(), "ids":rel_samples}
+    return res, pol_dist, np.sum(sample_dist >= 1e-4), info
 
 def run_all(args, samples):
     print("Running code for robust optimal policy \n --------------------")
@@ -362,9 +466,9 @@ def run_all(args, samples):
 
         N = args["num_samples"]
   
-        res_sg, pol_sg, sg_hist, active_sg = solve_subgrad(samples, model, max_iters=args["sg_itts"])
+        res_sg, pol_sg, active_sg, info_sg = solve_subgrad(samples, model, max_iters=args["sg_itts"])
         sg_active_num = active_sg.size 
-        plt.loglog(sg_hist)
+        plt.loglog(info_sg["hist"])
 
         print("Using subgradient methods found " + str(active_sg.size) + " active constraints a posteriori")
         [a_post_eps_L, a_post_eps_U] = \
@@ -375,8 +479,8 @@ def run_all(args, samples):
 
         print("Optimal satisfaction probability is found to be {:.3f}".format(res_sg))
         if len(model.States)**len(model.Actions) < 200:
-            res_MNE, pol_MNE, pols = MNE_solver(samples, model)
-            res_FSP, pol_FSP, pols, a_post_support_num = FSP_solver(samples, model, max_iters=args["FSP_itts"])
+            res_MNE, pol_MNE, info_MNE = MNE_solver(samples, model)
+            res_FSP, pol_FSP, a_post_support_num, info_FSP = FSP_solver(samples, model, max_iters=args["FSP_itts"])
             if active_sg.size != a_post_support_num:
                 print("Found {} supports using subgradient method, but {} using fictitious self play".format(active_sg.size, a_post_support_num))
             print("----------------\nResult comparison:\nmatrix solver: {:.13f}\nFSP: {:.13f}\nSubgradient: {:.13f}".format(res_MNE, res_FSP, res_sg))
@@ -387,6 +491,7 @@ def run_all(args, samples):
             print("Hence, a posteriori, violation probability is in the range [{:.3f}, {:.3f}], with confidence {:.3f}"
                 .format(a_post_eps_L, a_post_eps_U, args["beta"]))
             print("Optimal satisfaction probability is found to be {:.3f}".format(res_MNE))
+        import pdb; pdb.set_trace()
         if args["result_save_file"] is not None:
             save_data(args["result_save_file"], {"worst": wc, "probs":probs, "pol":pol_sg, "supports":active_sg})
 
@@ -413,10 +518,10 @@ def test_support_num(args):
     num_underestimates = 0
     for i in range(10):
         print("-----------------\nGenerating new sample batch")
-        samples = get_samples(model, args["num_samples"])
+        samples = get_samples(args)
 
         #probs, pol, supports, a_post_support_num, all_p = calc_probs_policy_iteration(model, base, samples)
-        res_sg, pol_sg, sg_hist, active_sg = solve_subgrad(samples, model, max_iters=100, quiet=True)
+        res_sg, pol_sg, active_sg, _ = solve_subgrad(samples, model, max_iters=100, quiet=True)
          
         print("Calculated supports: " + str(active_sg+1)) # Add 1 for 1 indexign
         
