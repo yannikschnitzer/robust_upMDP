d752226609ca1333da8408628f009536b304cfa4
lukearcus <rickard@robots.ox.ac.uk>
2023-11-13 12:47:17 +0000

    Removed repeated runs

--- main/solvers.py
+++ main/solvers.py
@@ -296,131 +296,113 @@ class subgrad(optimiser):
         sol_ress = []
         all_active = []
         alternative_supps = set()
-        for z in range(num_start_points):
-            pol = np.zeros((num_states, num_acts))
-            params = np.ones((1, num_acts))*-np.infty
-            for s in model.States:
-                for a in model.Enabled_actions[s]:
-                    params[0,a] = np.random.random()#1/len(model.Enabled_actions[s])
-                    #pol[s,a] = 1/len(model.Enabled_actions[s])
-                pol[s,:] = softmax(params[0,:])
-            
-            wc_hist = []
-            best_hist = []
-            tic = time.perf_counter()
-            
-            if num_start_points == 1:
-                for s in model.States:
-                    for a in model.Enabled_actions[s]:
-                        pol[s,a] = 1/len(model.Enabled_actions[s])
-                wc, true_probs, _ = self.test_pol(model, samples, pol, paramed_models = sample_trans_probs)
-                
-                worst = np.argwhere(true_probs[:,model.Init_state]==wc)
-                worst = np.random.choice(worst.flatten())
-                toc = time.perf_counter()
-                logging.debug("Time for finding worst case: {:.3f}s".format(toc-tic)) # This is also done every iteration, could be sped up but takes ~6/1500 the time
-                best_worst_pol = self.test_pol(model, [samples[worst]])[2][0]
-                test_wc, test_probs, _ = self.test_pol(model, samples, best_worst_pol, paramed_models = sample_trans_probs)
-                test_worst = np.argwhere(test_probs[:,model.Init_state]==test_wc).flatten()
-                if worst in test_worst:
-                    info = {"hist":[test_wc], "all":test_probs[:, model.Init_state]}
-                    if not self.quiet:
-                        print("Worst case holds with deterministic policy, deterministic is optimal")
-                    return test_wc, best_worst_pol, test_worst, info
-                pol = 0.1*pol + 0.9*best_worst_pol # a nicer start point
-            
-            wc, true_probs, _ = self.test_pol(model, samples, pol, paramed_models = sample_trans_probs)
-            worst = np.argwhere(true_probs[:,model.Init_state]==wc)
-            worst = np.random.choice(worst.flatten())
+        pol = np.zeros((num_states, num_acts))
+        
+        wc_hist = []
+        best_hist = []
+        tic = time.perf_counter()
+        
+        for s in model.States:
+            for a in model.Enabled_actions[s]:
+                pol[s,a] = 1/len(model.Enabled_actions[s])
+        wc, true_probs, _ = self.test_pol(model, samples, pol, paramed_models = sample_trans_probs)
+        
+        worst = np.argwhere(true_probs[:,model.Init_state]==wc)
+        worst = np.random.choice(worst.flatten())
+        toc = time.perf_counter()
+        logging.debug("Time for finding worst case: {:.3f}s".format(toc-tic)) # This is also done every iteration, could be sped up but takes ~6/1500 the time
+        best_worst_pol = self.test_pol(model, [samples[worst]])[2][0]
+        test_wc, test_probs, _ = self.test_pol(model, samples, best_worst_pol, paramed_models = sample_trans_probs)
+        test_worst = np.argwhere(test_probs[:,model.Init_state]==test_wc).flatten()
+        if worst in test_worst:
+            info = {"hist":[test_wc], "all":test_probs[:, model.Init_state]}
+            if not self.quiet:
+                print("Worst case holds with deterministic policy, deterministic is optimal")
+            return test_wc, best_worst_pol, test_worst, info
+        pol = 0.1*pol + 0.9*best_worst_pol # a nicer start point
+        
+        wc, true_probs, _ = self.test_pol(model, samples, pol, paramed_models = sample_trans_probs)
+        worst = np.argwhere(true_probs[:,model.Init_state]==wc)
+        worst = np.random.choice(worst.flatten())
     
+        if model.opt == "max":
+            best = 0
+        else:
+            best = 1
+    
+        best_pol = pol
+        for i in tqdm(range(self.max_iters)):
+            if self.check_timeout(start):
+                break
+                #return -1, None, None, None
+            time_start = time.perf_counter()
+            
+            old_pol = np.copy(pol)
+            step = self.init_step/((i+1)**self.step_exp)
+            #step = 1/((i+1)**(1/2))
+            #step = 0.1
+            alternative_supps.add(worst)
+            grad = self.find_grad(model, pol, samples[worst])
+            #grad_norm = np.linalg.norm(grad, ord=np.inf)
+             
+            time_grads = time.perf_counter()-time_start
+            #if time_grads > 2:
+            #    import pdb; pdb.set_trace()
+            logging.debug("Total time for finding gradients: {:.3f}".format(time_grads))
             if model.opt == "max":
-                best = 0
+                pol += step*grad
             else:
-                best = 1
+                pol -= step*grad 
     
-            best_pol = pol
-            for i in tqdm(range(self.max_iters)):
-                if self.check_timeout(start):
-                    break
-                    #return -1, None, None, None
-                time_start = time.perf_counter()
-                
-                old_pol = np.copy(pol)
-                step = self.init_step/((i+1)**self.step_exp)
-                #step = 1/((i+1)**(1/2))
-                #step = 0.1
-                alternative_supps.add(worst)
-                grad = self.find_grad(model, pol, samples[worst])
-                #grad_norm = np.linalg.norm(grad, ord=np.inf)
-                 
-                time_grads = time.perf_counter()-time_start
-                #if time_grads > 2:
-                #    import pdb; pdb.set_trace()
-                logging.debug("Total time for finding gradients: {:.3f}".format(time_grads))
-                if model.opt == "max":
-                    pol += step*grad
+            for s in model.States:
+                fin = False
+                if len(model.Enabled_actions[s]) <= 1:
+                    pass
                 else:
-                    pol -= step*grad 
-    
-                for s in model.States:
-                    fin = False
-                    if len(model.Enabled_actions[s]) <= 1:
-                        pass
-                    else:
-                        acts = model.Enabled_actions[s]
+                    acts = model.Enabled_actions[s]
+                    normal = np.ones(len(acts))/len(acts)**(1/2)
+                    orig = np.ones(len(acts))/len(acts)
+                    pol_s_a = pol[s][acts]
+                    v = pol_s_a - orig
+                    dist = np.dot(v,normal)
+                    proj = pol_s_a - dist*normal
+                    while True:
                         normal = np.ones(len(acts))/len(acts)**(1/2)
                         orig = np.ones(len(acts))/len(acts)
                         pol_s_a = pol[s][acts]
                         v = pol_s_a - orig
                         dist = np.dot(v,normal)
                         proj = pol_s_a - dist*normal
-                        while True:
-                            normal = np.ones(len(acts))/len(acts)**(1/2)
-                            orig = np.ones(len(acts))/len(acts)
-                            pol_s_a = pol[s][acts]
-                            v = pol_s_a - orig
-                            dist = np.dot(v,normal)
-                            proj = pol_s_a - dist*normal
-                            if np.all(proj > 0):
-                                break
-                            else:
-                                pos = np.where(proj>0)[0]
-                                acts = [acts[p] for p in pos]
-                        pol[s] = 0
-                        pol[s][acts] = proj
-                time_proj = time.perf_counter()-time_start-time_grads
-                logging.debug("Time for projection step: {:.3f}".format(time_proj))
-                wc, true_probs, _ = self.test_pol(model, samples, pol, paramed_models = sample_trans_probs) # This is taking a little while? (like 1 min, could probably speed up)
-                worst = np.argwhere(true_probs[:,model.Init_state]==wc)
-                worst = np.random.choice(worst.flatten())
-                wc_hist.append(wc)
-                if model.opt == "max":
-                    if wc > best:
-                        best = wc
-                        best_pol = pol
-                else:
-                    if wc < best:
-                        best = wc
-                        best_pol = pol
-                best_hist.append(best)
-                logging.info("Iteration: {}".format(i+1))
-                logging.info("Current value: {:.6f}, with sample {}".format(wc, worst))
-                #logging.info("Policy inf norm change: {:.3f}".format(np.linalg.norm(pol-old_pol, ord=np.inf)))
-                if len(wc_hist) >= 2:
-                    change=abs(wc_hist[-2]-wc_hist[-1])
-                    logging.info("Value change: {:.6f}".format(change))
-                    if change < self.tol:
-                        break
-            sol_pols.append(best_pol)
-            sol_ress.append(best)
-        
-            wc, true_probs, _ = self.test_pol(model, samples, best_pol, paramed_models = sample_trans_probs)
+                        if np.all(proj > 0):
+                            break
+                        else:
+                            pos = np.where(proj>0)[0]
+                            acts = [acts[p] for p in pos]
+                    pol[s] = 0
+                    pol[s][acts] = proj
+            time_proj = time.perf_counter()-time_start-time_grads
+            logging.debug("Time for projection step: {:.3f}".format(time_proj))
+            wc, true_probs, _ = self.test_pol(model, samples, pol, paramed_models = sample_trans_probs) # This is taking a little while? (like 1 min, could probably speed up)
+            worst = np.argwhere(true_probs[:,model.Init_state]==wc)
+            worst = np.random.choice(worst.flatten())
+            wc_hist.append(wc)
             if model.opt == "max":
-                active_sg = np.argwhere(true_probs[:, model.Init_state] <= wc+self.supp_tol) # this tol is hard to tune...
+                if wc > best:
+                    best = wc
+                    best_pol = pol
             else:
-                active_sg = np.argwhere(true_probs[:, model.Init_state] >= wc-self.supp_tol)
-            all_active.append(active_sg)
-        import pdb; pdb.set_trace()
+                if wc < best:
+                    best = wc
+                    best_pol = pol
+            best_hist.append(best)
+            logging.info("Iteration: {}".format(i+1))
+            logging.info("Current value: {:.6f}, with sample {}".format(wc, worst))
+            #logging.info("Policy inf norm change: {:.3f}".format(np.linalg.norm(pol-old_pol, ord=np.inf)))
+            if len(wc_hist) >= 2:
+                change=abs(wc_hist[-2]-wc_hist[-1])
+                logging.info("Value change: {:.6f}".format(change))
+                if change < self.tol:
+                    break
         final_time = time.perf_counter() - start
         print("Subgradient Descent took {:.3f}s".format(final_time))
         wc, true_probs, _ = self.test_pol(model, samples, best_pol, paramed_models = sample_trans_probs)
