321211688ebe29693613ffd5e32d342c57cbdb7c
lukearcus <rickard@robots.ox.ac.uk>
2023-11-14 12:21:51 +0000

    Changed markers

--- plot_runtimes.py
+++ plot_runtimes.py
@@ -14,7 +14,7 @@ for elem in res_states:
     list_times = [time[0] for time in res_states[elem] if len(time) > 0]
     list_times = [time for time in list_times if time < 3600] 
     x_vals = MDP_sizes[:len(list_times)] 
-    ax.loglog(x_vals, list_times, marker='+', linestyle="--", label=elem)
+    ax.loglog(x_vals, list_times, marker='x', linestyle="--", label=elem)
 ax.legend(loc="upper left")
 ax.set_xlabel(r'MDP size $|\mathcal{S}|\cdot|\mathcal{A}|$')
 ax.set_ylabel("runtime (seconds)")
@@ -28,7 +28,7 @@ for elem in res[1]:
     list_times = [time[0] for time in res[1][elem] if len(time) > 0]
     list_times = [time for time in list_times if time < 3600] 
     x_vals = num_samples[:len(list_times)] 
-    ax.loglog(x_vals, list_times, marker='+', linestyle="--", label=elem)
+    ax.loglog(x_vals, list_times, marker='x', linestyle="--", label=elem)
 ax.legend(loc="upper left")
 ax.set_xlabel('Number of Samples')
 ax.set_ylabel("runtime (seconds)")
