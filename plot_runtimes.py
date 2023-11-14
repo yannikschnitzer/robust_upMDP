3059d26bd3cc077ffb347b248a1ac3ae0b9120c9
lukearcus <rickard@robots.ox.ac.uk>
2023-11-14 12:23:16 +0000

    Moved legend

--- plot_runtimes.py
+++ plot_runtimes.py
@@ -15,7 +15,7 @@ for elem in res_states:
     list_times = [time for time in list_times if time < 3600] 
     x_vals = MDP_sizes[:len(list_times)] 
     ax.loglog(x_vals, list_times, marker='x', linestyle="--", label=elem)
-ax.legend(loc="upper left")
+ax.legend(loc="upper right")
 ax.set_xlabel(r'MDP size $|\mathcal{S}|\cdot|\mathcal{A}|$')
 ax.set_ylabel("runtime (seconds)")
 ax.set_title("Runtime vs MDP size")
@@ -29,7 +29,7 @@ for elem in res[1]:
     list_times = [time for time in list_times if time < 3600] 
     x_vals = num_samples[:len(list_times)] 
     ax.loglog(x_vals, list_times, marker='x', linestyle="--", label=elem)
-ax.legend(loc="upper left")
+ax.legend(loc="upper right")
 ax.set_xlabel('Number of Samples')
 ax.set_ylabel("runtime (seconds)")
 ax.set_title("Runtime vs Sample Size")
