429925fa0bd32719ddb56db5363dd9a7b72d7add
lukearcus <rickard@robots.ox.ac.uk>
2023-11-14 12:13:04 +0000

    Changed to loglog

--- plot_runtimes.py
+++ plot_runtimes.py
@@ -14,7 +14,7 @@ for elem in res_states:
     list_times = [time[0] for time in res_states[elem] if len(time) > 0]
     list_times = [time for time in list_times if time < 3600] 
     x_vals = MDP_sizes[:len(list_times)] 
-    ax.semilogy(list_times, x_vals, label=elem)
+    ax.loglog(x_vals, list_times, label=elem)
 ax.legend(loc="upper left")
 ax.set_xlabel(r'MDP size $|\mathcal{S}|\cdot|\mathcal{A}|$')
 ax.set_ylabel("runtime (seconds)")
