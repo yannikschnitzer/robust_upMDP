2f694fb9acff4796e19c3523d88f4c21a84c281a
lukearcus <rickard@robots.ox.ac.uk>
2023-11-15 15:46:08 +0000

    plot range of runtimes

--- plot_runtimes.py
+++ plot_runtimes.py
@@ -1,5 +1,6 @@
 import pickle
 import matplotlib.pyplot as plt
+import numpy as np
 
 with open("runtime_res.pkl", "rb") as f:
     res = pickle.load(f)
@@ -11,26 +12,36 @@ MDP_sizes = [(num_states*2+2) for num_states in MDP_sizes]
 
 fig, ax = plt.subplots()
 for elem in res_states:
-    list_times = [time[0] for time in res_states[elem] if len(time) > 0]
-    list_times = [time for time in list_times if time < 3600] 
+    list_times = [time for time in res_states[elem] if len(time) > 0]
+    #list_times = [[time for time in times if time < 3600] for times in list_times] 
+    
     x_vals = MDP_sizes[:len(list_times)] 
-    ax.loglog(x_vals, list_times, marker='x', linestyle="--", label=elem)
+    list_times = np.array(list_times).T
+    min_times = np.min(list_times, axis=0)
+    max_times = np.max(list_times, axis=0)
+    mean_times = np.mean(list_times, axis=0)
+    ax.fill_between(x_vals, min_times, max_times)
+    ax.loglog(x_vals, mean_times, marker='x', linestyle="--", label=elem)
+    
+    #ax.boxplot(list_times, vert=True, labels=x_vals)
+    
+    #ax.loglog(x_vals, list_times, marker='x', linestyle="--", label=elem)
 ax.legend(loc="upper right")
 ax.set_xlabel(r'MDP size $|\mathcal{S}|\cdot|\mathcal{A}|$')
 ax.set_ylabel("runtime (seconds)")
 ax.set_title("Runtime vs MDP size")
 plt.savefig("runtimes_plot_states.pdf")
 
-num_samples = list(range(100,1000,50))
-
-fig, ax = plt.subplots()
-for elem in res[1]:
-    list_times = [time[0] for time in res[1][elem] if len(time) > 0]
-    list_times = [time for time in list_times if time < 3600] 
-    x_vals = num_samples[:len(list_times)] 
-    ax.loglog(x_vals, list_times, marker='x', linestyle="--", label=elem)
-ax.legend(loc="upper right")
-ax.set_xlabel('Number of Samples')
-ax.set_ylabel("runtime (seconds)")
-ax.set_title("Runtime vs Sample Size")
-plt.savefig("runtimes_plot_samples.pdf")
+#num_samples = list(range(100,1000,50))
+#
+#fig, ax = plt.subplots()
+#for elem in res[1]:
+#    list_times = [time for time in res[1][elem] if len(time) > 0]
+#    list_times = [[time for time in times if time < 3600] for times in list_times] 
+#    x_vals = num_samples[:len(list_times)] 
+#    ax.loglog(x_vals, list_times, marker='x', linestyle="--", label=elem)
+#ax.legend(loc="upper right")
+#ax.set_xlabel('Number of Samples')
+#ax.set_ylabel("runtime (seconds)")
+#ax.set_title("Runtime vs Sample Size")
+#plt.savefig("runtimes_plot_samples.pdf")
