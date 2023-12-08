b781a7601442853ab65a43eef4f25a7f0ddb4c93
lukearcus <rickard@robots.ox.ac.uk>
2023-12-08 12:52:27 +0000

    long test figs

--- plots.sh
+++ plots.sh
@@ -1,2 +1,2 @@
-python run.py --model test7 --save_figs test_figs -N 200
+python run.py --model test7 --save_figs test_figs -N 200 --tol 0
 #python run.py --sg_itt 1000 --init_step 1000 --model drone --inst uniform -vo --file_out drone_uniform.txt --tol 1e-4 --save_res drone_uniform_100.txt -N 200 --to 172800 --MC --MC_p --sg_only --save_figs drone_uniform --save uniform_samples.pkl
