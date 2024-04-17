487ad9cc6afed8ad81b3a8fe1fd16be91159b122
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2024-04-17 16:45:46 +0100

    update

--- benchmarks.sh
+++ benchmarks.sh
@@ -6,7 +6,7 @@
 #python run.py --sg_itt 2500 --init_step 1000 --model brp --inst 256,5 -vo --file_out brp_25615.txt --tol 1e-4 --save_res brp_25615_res.txt -N 200 --to 3600 --MC --MC_p --sg_only --MC_samples 1000
 #python run.py --sg_itt 2500 --init_step 1000 --model brp --inst 4096,5 -vo --file_out brp_40965.txt --tol 1e-4 --save_res brp_40965_res.txt -N 200 --to 3600 --MC --MC_p --sg_only --MC_samples 1000
 
-python3 run.py --sg_itt 2500 --init_step 10 --model sav --inst 2,6,2,2 -vo --file_out sav_2622.txt --tol 1e-4 --save_res sav_2622_res.txt -N 20 --to 3600 --MC --MC_p --sg_only --MC_samples 10
+python3 run.py --sg_itt 2500 --init_step 1000 --model sav --inst 2,6,2,2 -vo --file_out sav_2622.txt --tol 1e-4 --save_res sav_2622_res.txt -N 20 --to 3600 --MC --MC_p --sg_only --MC_samples 10
 #python3 run.py --sg_itt 2500 --init_step 1000 --model sav --inst 2,100,10,10 -vo --file_out sav_21001010.txt --tol 1e-4 --save_res sav_21001010_res.txt -N 200 --to 3600 --MC --MC_p --sg_only --MC_samples 1000
 #python run.py --sg_itt 2500 --init_step 1000 --model sav --inst 4,6,2,2 -vo --file_out sav_3622.txt --tol 1e-4 --save_res sav_4622_res.txt -N 200 --to 3600 --MC --MC_p --sg_only --MC_samples 1000
 #python run.py --sg_itt 2500 --init_step 1000 --model sav --inst 4,10,3,3 -vo --file_out sav_41033.txt --tol 1e-4 --save_res sav_41033_res.txt -N 200 --to 3600 --MC --MC_p --sg_only --MC_samples 1000
